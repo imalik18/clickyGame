6e5b1be3457772e943c4c7d4d49e12419af025af
Iman Malik <imalik92@gmail.com>
2019-02-16 13:41:49 -0600

    Updates

---
+++ precache-manifest.ea31a650da388d92575a6fd353d11e9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickyGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3fe81c4c31232682d87b",
+    "url": "/clickyGame/static/js/runtime~main.3fe81c4c.js"
+  },
+  {
+    "revision": "5aa048bbf81c857592cb",
+    "url": "/clickyGame/static/js/main.5aa048bb.chunk.js"
+  },
+  {
+    "revision": "7ce48e387d423de40399",
+    "url": "/clickyGame/static/js/2.7ce48e38.chunk.js"
+  },
+  {
+    "revision": "5aa048bbf81c857592cb",
+    "url": "/clickyGame/static/css/main.4dc9b9c5.chunk.css"
+  },
+  {
+    "revision": "4c6d15e709c89e627353fc27de8faa18",
+    "url": "/clickyGame/index.html"
+  }
+];
